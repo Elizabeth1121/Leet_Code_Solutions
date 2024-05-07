1c3a0ee0303ac523ab85255f46b6f939617eb904
Elizabeth <106032842+Elizabeth1121@users.noreply.github.com>
2024-05-07 12:41:17 -0400

    Add files via upload
    Final Value After Operations Challenge

---
+++ Final_Value_Of_Variable/Program.cs
@@ -0,0 +1,30 @@
+﻿Solution solution = new Solution();
+string[] operations = new string[] { "X++", "++X", "--X"};
+Console.WriteLine(solution.FinalValueAfterOperations(operations));
+
+
+public class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int value = 0;
+
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case "X++":
+                case "++X":
+                    value++;
+                    break;
+
+                case "X--":
+                case "--X":
+                    value--;
+                    break;
+            }
+        }
+
+        return value;
+    }
+}
